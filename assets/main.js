ec48e64f9fb14d8e8f5dd09674f8eef49e960dca
Juzbox <hugopilate@gmail.com>
2021-07-04 13:57:16 +0200

    deleteUpdate

--- assets/main.js
+++ assets/main.js
@@ -33,7 +33,7 @@ function setup() {
       col4[i] = data.getString(i, 3);//method
     }
   }
-  word1 = random(col1) + " " + random(col3);
+  word1 = random(col1);
   word2 = random(col4);
   word3 = random(col2);
 }
@@ -59,36 +59,36 @@ function Type1() {
   document.getElementsByClassName('x1')[0].innerHTML = text1;
   _PART_INDEX_X++;
 
-  if(s%18==1) {
-    clearInterval(_INTERVAL_VAL_X);
-    setTimeout(function() {
-      _INTERVAL_VAL_X = setInterval(Delete1, 50);
-    }, 1000);
-  }
+  // if(s%18==1) {
+  //   clearInterval(_INTERVAL_VAL_X);
+  //   setTimeout(function() {
+  //     _INTERVAL_VAL_X = setInterval(Delete1, 50);
+  //   }, 1000);
+  // }
 }
 
-function Delete1() {
-  var text1 =  word1.substring(0, _PART_INDEX_X - 1);
-  document.getElementsByClassName('x1')[0].innerHTML = text1;
-  _PART_INDEX_X--;
+// function Delete1() {
+//   var text1 =  word1.substring(0, _PART_INDEX_X - 1);
+//   document.getElementsByClassName('x1')[0].innerHTML = text1;
+//   _PART_INDEX_X--;
 
-  if(text1 === '') {
-    word1 = random(col1)+" "+random(col3);
+//   if(text1 === '') {
+//     word1 = random(col1)+" "+random(col3);
 
-    clearInterval(_INTERVAL_VAL_X);
+//     clearInterval(_INTERVAL_VAL_X);
 
-    if(_PARTX == (word1.length - 1))
-    _PARTX = 0;
-    else
-    _PARTX++;
+//     if(_PARTX == (word1.length - 1))
+//     _PARTX = 0;
+//     else
+//     _PARTX++;
 
-    _PART_INDEX_X = 0;
+//     _PART_INDEX_X = 0;
 
-    setTimeout(function() {
-      _INTERVAL_VAL_X = setInterval(Type1, 100);
-    }, 200);
-  }
-}
+//     setTimeout(function() {
+//       _INTERVAL_VAL_X = setInterval(Type1, 100);
+//     }, 200);
+//   }
+// }
 
 /*----------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -97,36 +97,36 @@ function Type2() {
   document.getElementsByClassName('y1')[0].innerHTML = text2;
   _PART_INDEX++;
 
-  if(s%18 == 1) {
-    clearInterval(_INTERVAL_VAL);
-    setTimeout(function() {
-      _INTERVAL_VAL= setInterval(Delete2, 50);
-    }, 1000);
-  }
+  // if(s%18 == 1) {
+  //   clearInterval(_INTERVAL_VAL);
+  //   setTimeout(function() {
+  //     _INTERVAL_VAL= setInterval(Delete2, 50);
+  //   }, 1000);
+  // }
 }
 
-function Delete2() {
-  var text2 =  word2.substring(0, _PART_INDEX - 1);
-  document.getElementsByClassName('y1')[0].innerHTML = text2;
-  _PART_INDEX--;
+// function Delete2() {
+//   var text2 =  word2.substring(0, _PART_INDEX - 1);
+//   document.getElementsByClassName('y1')[0].innerHTML = text2;
+//   _PART_INDEX--;
 
-  if(text2 === '') {
-    word2 = random(col4);
+//   if(text2 === '') {
+//     word2 = random(col4);
 
-    clearInterval(_INTERVAL_VAL);
+//     clearInterval(_INTERVAL_VAL);
 
-    if(_PART == (word2.length - 1))
-    _PART = 0;
-    else
-    _PART++;
+//     if(_PART == (word2.length - 1))
+//     _PART = 0;
+//     else
+//     _PART++;
 
-    _PART_INDEX = 0;
+//     _PART_INDEX = 0;
 
-    setTimeout(function() {
-      _INTERVAL_VAL= setInterval(Type2, 100);
-    }, 200);
-  }
-}
+//     setTimeout(function() {
+//       _INTERVAL_VAL= setInterval(Type2, 100);
+//     }, 200);
+//   }
+// }
 
 /*----------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -135,36 +135,36 @@ function Type3() {
   document.getElementsByClassName('z1')[0].innerHTML = text3;
   _PART_INDEX_Y++;
 
-  if(s%18 == 1) {
-    clearInterval(_INTERVAL_VAL_Y);
-    setTimeout(function() {
-      _INTERVAL_VAL_Y= setInterval(Delete3, 50);
-    }, 1000);
-  }
+  // if(s%18 == 1) {
+  //   clearInterval(_INTERVAL_VAL_Y);
+  //   setTimeout(function() {
+  //     _INTERVAL_VAL_Y= setInterval(Delete3, 50);
+  //   }, 1000);
+  // }
 }
 
-function Delete3() {
-  var text3 =  word3.substring(0, _PART_INDEX_Y - 1);
-  document.getElementsByClassName('z1')[0].innerHTML = text3;
-  _PART_INDEX_Y--;
+// function Delete3() {
+//   var text3 =  word3.substring(0, _PART_INDEX_Y - 1);
+//   document.getElementsByClassName('z1')[0].innerHTML = text3;
+//   _PART_INDEX_Y--;
 
-  if(text3 === '') {
-    word3 = random(col2);
+//   if(text3 === '') {
+//     word3 = random(col2);
 
-    clearInterval(_INTERVAL_VAL_Y);
+//     clearInterval(_INTERVAL_VAL_Y);
 
-    if(_PARTY == (word3.length - 1))
-    _PARTY = 0;
-    else
-    _PARTY++;
+//     if(_PARTY == (word3.length - 1))
+//     _PARTY = 0;
+//     else
+//     _PARTY++;
 
-    _PART_INDEX_Y = 0;
+//     _PART_INDEX_Y = 0;
 
-    setTimeout(function() {
-      _INTERVAL_VAL_Y= setInterval(Type3, 100);
-    }, 300);
-  }
-}
+//     setTimeout(function() {
+//       _INTERVAL_VAL_Y= setInterval(Type3, 100);
+//     }, 300);
+//   }
+// }
 _INTERVAL_VAL_X = setInterval(Type1, 100);
 _INTERVAL_VAL_Y = setInterval(Type3, 100);
 _INTERVAL_VAL= setInterval(Type2, 100);
